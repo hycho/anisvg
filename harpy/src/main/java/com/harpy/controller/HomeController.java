b0e2665fd5751de5c81440c737ac6cfbf578222e
kofwhgh <kofwhgh@e0dc6016-3523-b513-d90a-692efce16c35>
2014-05-15 05:09:25 +0000

    Logback 설정 완료

--- harpy/src/main/java/com/harpy/controller/HomeController.java
+++ harpy/src/main/java/com/harpy/controller/HomeController.java
@@ -1,13 +1,22 @@
 package com.harpy.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
+	
+  static final Logger log = LoggerFactory.getLogger(HomeController.class);
+  
   @RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
+	  log.info("Home Action Info Log Test");
+	  log.debug("Home Action Debug Log Test");
+	  log.error("Home Action Error Log Test");
+	  
 	  return "/index";
 	}
 }
